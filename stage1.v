ff28ff95c2f176bf3f43319f20dd269211387120
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-05 20:14:17 +0530

    stage 1

--- stage1.v
+++ stage1.v
@@ -8,30 +8,27 @@ module stage1(
     output reg done,
     output reg [15:0] stg1_out
 );
-    reg [15:0] shift_reg;
+    reg [15:0] shift_reg = 0;
 
     always @(posedge clk1 or posedge rst) 
     begin
         if (rst) 
         begin
             shift_reg <= 16'b0;
+            stg1_out <= 16'b0;
             done <= 1'b0;
         end 
-        else 
+        else
         begin
             if (ld) 
             begin
                 shift_reg <= input_data;
                 done <= 1'b0;
             end 
-            else if (start) 
+            else if (start & ~done) 
             begin
                 stg1_out <= (shift_reg << key_bits[4:2]) + 16'd3;
                 done <= 1'b1;
-            end 
-            else 
-            begin
-                done <= 1'b0;
             end
         end
     end
