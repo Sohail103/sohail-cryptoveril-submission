880260af8532bf651c312fffacf2f1bd10579ba8
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-05 17:11:46 +0530

    stage 1

--- stage1.v
+++ stage1.v
@@ -1,10 +1,38 @@
 module stage1(
     input wire clk1,
+    input wire rst,
     input wire [4:0] key_bits,
     input wire [15:0] input_data,
     input wire ld,
-    output wire done,
-    output wire [15:0] stg1_out
+    input wire start,
+    output reg done,
+    output reg [15:0] stg1_out
 );
     reg [15:0] shift_reg;
-    
+
+    always @(posedge clk1 or posedge rst) 
+    begin
+        if (rst) 
+        begin
+            shift_reg <= 16'b0;
+            done <= 1'b0;
+        end 
+        else 
+        begin
+            if (ld) 
+            begin
+                shift_reg <= input_data;
+                done <= 1'b0;
+            end 
+            else if (start) 
+            begin
+                stg1_out <= (shift_reg << key_bits[4:2]) + 16'd3;
+                done <= 1'b1;
+            end 
+            else 
+            begin
+                done <= 1'b0;
+            end
+        end
+    end
+endmodule
