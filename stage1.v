83de1fc6f7fac62ca783fa4ab08e8735ecaeb352
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-05 16:24:57 +0530

    stage 1

--- stage1.v
+++ stage1.v
@@ -2,7 +2,9 @@ module stage1(
     input wire clk1,
     input wire [4:0] key_bits,
     input wire [15:0] input_data,
+    input wire ld,
+    output wire done,
     output wire [15:0] stg1_out
 );
-    
+    reg [15:0] shift_reg;
     
